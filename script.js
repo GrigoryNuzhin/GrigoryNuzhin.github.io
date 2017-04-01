2b459e29319944edcfdf7e3024913dd91aae461a
Григорий Нужин <grigory.nuzhin@gmail.com>
2017-04-01 21:23:11 +0500

    Язычок бета V1. Настроена анимация показ/скрытие текста. В текущей ситуации оказалось проще использовать jQuery анимацию.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var showDisappear = $('.content > div .disappear'),
     divs = showDisappear.children('div'),
     DISAPPEARHEIGHT = 63;
@@ -5,24 +6,24 @@ var showDisappear = $('.content > div .disappear'),
 for (var i = 0; i < divs.length; i++) {
     divs[i].parentElement.className = "show";
     divs[i].defaultHeight = divs[i].offsetHeight + "px";
-    divs[i].style.height = DISAPPEARHEIGHT + "px";    
+    divs[i].style.height = DISAPPEARHEIGHT + "px";
     divs[i].parentElement.className = "disappear";
 }
 
-showDisappear.children('div').wrapInner( "<div class='wrapper'></div>");
+showDisappear.children('div').wrapInner("<div class='wrapper'></div>");
 
 showDisappear.click(function() {
-    var div = $(this).children('div')[0];
+    var div = $(this).children('div')[0],
+        time = 500;
     if (this.className == "disappear") {
         this.className = "show";
+        $(div).animate({ height: div.defaultHeight }, time);
 
-        div.style.height = div.defaultHeight;
-       /*$(div).animate({height:div.defaultHeight},5000)*/;
-        /*this.style.height = div.defaultHeight;*/
     } else {
-        this.className = "disappear";
-        div.style.height = DISAPPEARHEIGHT + "px";
-        /*this.style.height = "0px";*/
-        
+
+        $(div).animate({ height: DISAPPEARHEIGHT + "px" }, time, (function() {
+            this.className = "disappear";
+        }).bind(this));
+
     }
 });
