3818162c38b664a307d79b5772e0cb5050544755
Григорий Нужин <grigory.nuzhin@gmail.com>
2017-04-08 18:53:06 +0500

    Анимации сообщений альфа V2. Конфликт CSS, исправлен временным решением. Теперь анимация срабатывает при прокрутке в одно направления, правда иногда бывает, что не так. Добавлено частичное решение блокировки прокрутки во время анимации из интернета, блокируется только колесика мыши, нужно придумать другое решение.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Функционал язычка */
 var showDisappear = $('.content > div .disappear'),
     divs = showDisappear.children('div'),
     DISAPPEARHEIGHT = 63, hide={};
@@ -10,15 +11,11 @@ for (var i = 0; i < divs.length; i++) {
     divs[i].style.height = DISAPPEARHEIGHT + "px";
     divs[i].parentElement.className = "disappear";
 }
-
-
-
 function fDisappear(elem1, elem2, time) {
     $(elem1).animate({ height: DISAPPEARHEIGHT + "px" }, time, (function() {
         elem2.className = "disappear";
     }));
 }
-
 showDisappear.on('click', function(e) {
     var div = $(this).children('div')[0],
         time = 500,
@@ -39,7 +36,6 @@ showDisappear.on('click', function(e) {
 
     }
 });
-
 $(document).on('click',function(e){
     if (!(e.target.tagName == 'MAIN' ||
     e.target.tagName == 'HTML' ||
@@ -52,23 +48,26 @@ $(document).on('click',function(e){
     fDisappear(hide.div, hide.saveThis, hide.time);
 });
 
+
+/* Автоматическая прокрутка */
 $('main > nav').click(function () { 
     destination = $('#footer').offset().top;
     $('html, body').animate({scrollTop: destination}, 1000); // HTML - необходимо для работы в браузере Safari!
+    $('.vh').removeClass('vh');
     return false;
 });
 
 
 
 
-
+/* Показ анимаций при прокрутке */
 var div = document.createElement("div");
 document.body.appendChild(div);
 div.className = "test"
 var x = "200px", y="100px", f
 
 
-window.onscroll = function() {
+function scrollEffects() {
     var footer = $('footer')[0],
         effectElem;
     x = footer.getBoundingClientRect().left + footer.offsetWidth / 2;
@@ -77,10 +76,110 @@ window.onscroll = function() {
     if (!~effectElem.className.indexOf("vh") ) return;    
     $(effectElem).removeClass("vh");
     $(effectElem).addClass("animated " + $(effectElem).data().classname);
-    /*div.style.top = y + "px";
-    div.style.left = x + "px";*/
-    // Footer не будем искать, будет легче узнать конец прокрутки, из-за того что у него фиксированая позиция.
+    // div.style.top = y + "px";
+    // div.style.left = x + "px";
+    // Footer не будем искать, будет легче узнать конец прокрутки, из-за того что у него фиксированная позиция.
+    new ScrollSwitch().off();
     console.log(document.elementFromPoint(x, y));
+    // Использовать событие анимация, нужно включать во время её overflo: hiden;
 }
 
+// http://getinstance.info/articles/javascript/css3-animation-javascript-event-handlers/
+var pfx = ["webkit", "moz", "MS", "o", ""];
+function PrefixedEvent(element, type, callback) {
+    for (var p = 0; p < pfx.length; p++) {
+        if (!pfx[p]) type = type.toLowerCase();
+        element.addEventListener(pfx[p] + type, callback, false);
+    }
+}
+
+$('.vh').each(function(index, pTransitionend){
+    PrefixedEvent(pTransitionend, "AnimationEnd", function(){
+        new ScrollSwitch().on();
+       setTimeout(function(){        
+       },500)
+  });
+})
+
 /*$(this).addClass($(this).data('animation'));*/
+
+
+/*$(window).scroll(function (){
+    $('.vh').each(function (){
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+$(window).height()-250) {
+            $(this).removeClass("vh");
+            $(this).addClass("animated " + $(this).data().classname);
+        }
+    });
+});﻿*/
+
+/* Временная заплатка конфликта тени в css. Удалить если удастся решить это другим способом.*/
+$(".vh").each(function(index, elem) {
+    if (index < 5) {
+        if (index % 2) {
+            $(elem).append("<span class='odd'></span>");
+        } else {
+            $(elem).append("<span class='even'></span>");
+        }
+    } else {
+        if (index % 2) {
+            $(elem).append("<span class='even'></span>");
+        } else {
+            $(elem).append("<span class='odd'></span>");
+        }
+    }
+
+});
+$(document).ready(function(){
+    setTimeout(function(){
+    window.scrollTo(0,0);
+    window.onscroll = scrollEffects; // Запуск происходит специально отдельно, для того, чтобы элементы случайно не раскрывались при автоматическом скролле случайно раскрытых элементов.
+    },50);
+});
+
+
+
+
+function ScrollSwitch(){
+    // http://stackoverflow.com/questions/4770025/how-to-disable-scrolling-temporarily/4770179#4770179
+    var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+    function preventDefault(e) {
+      e = e || window.event;
+      if (e.preventDefault)
+          e.preventDefault();
+      e.returnValue = false;  
+    }
+
+    function preventDefaultForScrollKeys(e) {
+        if (keys[e.keyCode]) {
+            preventDefault(e);
+            return false;
+        }
+    }
+
+    function disableScroll() {
+      if (window.addEventListener) // older FF
+          window.addEventListener('DOMMouseScroll', preventDefault, false);
+      window.onwheel = preventDefault; // modern standard
+      window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+      window.ontouchmove  = preventDefault; // mobile
+      document.onkeydown  = preventDefaultForScrollKeys;
+    }
+
+    function enableScroll() {
+        if (window.removeEventListener)
+            window.removeEventListener('DOMMouseScroll', preventDefault, false);
+        window.onmousewheel = document.onmousewheel = null; 
+        window.onwheel = null; 
+        window.ontouchmove = null;  
+        document.onkeydown = null;  
+    }
+
+    return {
+        off: disableScroll,
+        on: enableScroll
+    }
+}
